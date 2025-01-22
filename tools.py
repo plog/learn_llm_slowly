c23b6a9b0d0d83e07a29d0b6a720f89d3410d56a
Plog <plog@plog.net>
2025-01-22 21:59:12 +0800

    Refactor agent notebooks to replace ChatOllama with ChatGroq, enhance tool initialization, and improve execution output clarity

--- tools.py
+++ tools.py
@@ -1,15 +1,23 @@
 import os
 import langchain
 from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from qdrant_client import QdrantClient
 
 # Initialize the language model with specific settings
 OLLAMA_SERVER = os.getenv("OLLAMA_SERVER")
+
 llm = ChatOllama(
     base_url=os.getenv("OLLAMA_SERVER"),
     model = "llama3.2:latest",
     temperature = 0.8
 )
 
+groq = ChatGroq(
+    temperature=0, 
+    model_name="mixtral-8x7b-32768"
+)
+
 vdb = QdrantClient(host='lawboxai_qdrant')
+
